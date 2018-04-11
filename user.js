b25c873b329fb80514572321ebeccd5875a4c6fb
CatFish47 <chengxgrant@gmail.com>
2018-04-10 22:11:09 -0700

    Pusshhh

--- user.js
+++ user.js
@@ -4,7 +4,9 @@ var model = mongoose.model('user', new mongoose.Schema({
 	name: {type: String, unique: true}
 	, phone: {type: String, unique: true}
 	, email: {type: String}
+  , username: {type: String}
   , password: {type: String}
+  , salt: {type: String}
 }));
 
 exports.getModel = function() {
