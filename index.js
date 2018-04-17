14ac6f788508463b879ac16f144e2c58a0a7b8ab
CatFish47 <chengxgrant@gmail.com>
2018-04-16 17:37:46 -0700

    Add Stuff

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ var usermodel = require('./user.js').getModel();
 
 var crypto = require('crypto');
 
+var Io = require('socket.io');
+
 var dbAddress = process.env.MONGODB_URI || 'mongodb://127.0.0.1/spacecrash';
 
 /* Creates an express application */
@@ -23,14 +25,41 @@ var app = express();
 /* Creates the web server */
 var server = http.createServer(app);
 
+var io = Io(server);
+
 /* Defines what port to use to listen to web requests */
 var port =  process.env.PORT
 						? parseInt(process.env.PORT):
 						8080;
 
+function addSockets() {
+	io.on('connection', (socket) => {
+		console.log('user connected');
+	});
+}
+
+function authenticateUser(username, password, callback) {
+
+	if(!username) return callback('No username given');
+	if(!password) return callback('No password given');
+	usermodel.findOne({userName: username}, (err, user) => {
+		if(err) return callback('Error connecting to database');
+		if(!user) return callback('Incorrect username');
+		crypto.pbkdf2(password, user.salt, 10000, 256, 'sha256', (err, resp) => {
+			if(err) return callback('Error handling password');
+			if(resp.toString('base64') === user.password) return callback(null);
+			callback('Incorrect password');
+		});
+	});
+
+}
+
 function startServer() {
 
+	addSockets()
+
 	app.use(bodyParser.json({ limit: '16mb' }));
+	app.use(express.static(path.join(__dirname, 'public')));
 
 	/* Defines what function to call when a request comes from the path '/' in http://localhost:8080 */
 	app.get('/form', (req, res, next) => {
@@ -88,6 +117,41 @@ function startServer() {
 		res.send('OK');
 	})
 
+	app.get('/game', (req, res, next) => {
+		var filePath = path.join(__dirname, './game.html');
+
+		res.sendFile(filePath);
+	})
+
+	app.post('/game', (req, res, next) => {
+		console.log(req.body);
+		res.send('OK');
+	})
+
+	app.get('/login', (req, res, next) => {
+		var filePath = path.join(__dirname, './login.html');
+
+		res.sendFile(filePath);
+	})
+
+	app.post('/login', (req, res, next) => {
+		var username = req.body.userName;
+		var password = req.body.password;
+		authenticateUser(username, password, (err) => {
+			res.send({error: err});
+		});
+	});
+
+	io.on('connection', (socket) => {
+
+		console.log('user connected');
+
+		socket.on('disconnect', () => {
+			console.log('user disconnected');
+		})
+
+	});
+
 	/* Defines what function to all when the server recieves any request from http://localhost:8080 */
 	server.on('listening', () => {
 
@@ -100,6 +164,18 @@ function startServer() {
 
 		/* Outputs to the console that the webserver is ready to start listenting to requests */
 		console.log('Listening on ' + bind);
+	});
+
+	socket.on('message', (message) => {
+
+		console.log(message);
+
+	})
+
+	socket.on('message', (message) => {
+
+		io.emit('new message', message);
+
 	});
 
 	/* Tells the server to start listening to requests from defined port */
